3567427fac213e0dcdf4297b7e59ce15586fc9b9
xiaotian <952822399@qq.com>
2020-08-11 14:00:47 +0800

    init

--- App.js
+++ App.js
@@ -1,111 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import React from 'react';
-import { StyleSheet, Image, Platform, Text, View, TouchableOpacity } from 'react-native';
-import logo from './assets/logo.png'; 
-import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing'; 
-import uploadToAnonymousFilesAsync from 'anonymous-files'; 
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  StatusBar,
+} from 'react-native';
 
-export default function App() {
-  const [selectedImage, setSelectedImage] = React.useState(null);
+import {
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
+import { Container, Header, Item, Input, Body, Title, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Permission to access camera roll is required!");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
-
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    if (Platform.OS === 'web') {
-      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
-    } else {
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
-    } 
-
-    setSelectedImage({ localUri: pickerResult.uri });
-  }
-
-  let openShareDialogAsync = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
-      return;
-    }
-
-    await Sharing.shareAsync(selectedImage.localUri);
-  }; 
-
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+const App: () => React$Node = () => {
 
   return (
-    <View style={styles.container}>
-      {/* <Image source={logo} style={{ width: 305, height: 159 }} />  */}
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}  />
-
-      <Text style={styles.instructions}> To share a photo from your phone with a friend, just press the button below!</Text>
-      
-      <TouchableOpacity
-        onPress={openImagePickerAsync}
-        style={styles.button}>
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" />
+            <Icon name="ios-people" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
+        <Content>
+          <Grid>
+            <Col style={{ backgroundColor: '#635DB7', height: 200 }}></Col>
+          </Grid>
+          <Grid>
+            <Col style={{ backgroundColor: '#205DB7', height: 200 }}></Col>
+          </Grid>
+          <Grid>
+            <Col style={{ backgroundColor: '#635DB7', height: 200 }}></Col>
+          </Grid>
+          <Grid>
+            <Col style={{ backgroundColor: '#135DB7', height: 200 }}></Col>
+          </Grid>
+          <Grid>
+            <Col style={{ backgroundColor: '#935DB7', height: 200 }}></Col>
+          </Grid>
+          <Grid>
+            <Col style={{ backgroundColor: '#635DB7', height: 200 }}></Col>
+          </Grid>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button vertical>
+              <Icon name="apps" />
+              <Text>首页</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="apps" />
+              <Text>发现</Text>
+            </Button>
+            <Button vertical>
+              <Icon name="person" />
+              <Text>我的</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 10,
+  engine: {
+    position: 'absolute',
+    right: 0,
   },
-  instructions: {
-    color: '#888',
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
     fontSize: 18,
-    marginHorizontal: 15,
-  }, 
-  button: {
-    backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 5,
+    fontWeight: '400',
+    color: Colors.dark,
   },
-  buttonText: {
-    fontSize: 20,
-    color: '#fff',
-  }, 
-    /* Other styles hidden to keep the example brief... */
-    thumbnail: {
-      width: 300,
-      height: 300,
-      resizeMode: "contain"
-    }
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
 });
+
+
+export default App;
